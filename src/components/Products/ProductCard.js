0d93a496f706d3b293b1d3b0f721be6f4c7c5129
Krishna Sharma <ksharma10_be23@thapar.edu>
2024-08-14 00:40:58 +0530

    created a product card

---
+++ src/components/Products/ProductCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faHeart } from '@fortawesome/free-solid-svg-icons';
+import random from '../assets/random.jpg';
+
+const ProductCard = () => {
+    return (
+        <div className="bg-blue-100 w-72 min-h-[10rem] shadow-lg rounded-md overflow-hidden relative">
+            <img src={random} className="w-full h-full object-cover" alt="hello" />
+            <div className="p-5 flex flex-col gap-3">
+                <h2 className="font-semibold text-2xl">Headset</h2>
+                <span className="text-xl font-bold">
+                    Rs. 1000.00
+                </span>
+                <div className="mt-5 flex justify-between items-center gap-2">
+                    <button className="bg-blue-500/80 hover:bg-blue-500/90 px-6 py-2 rounded-md text-white font-medium tracking-wider transition flex items-center">
+                        Add to Cart
+                        <FontAwesomeIcon icon={faShoppingCart} className="ml-2" />
+                    </button>
+                </div>
+            </div>
+            <FontAwesomeIcon icon={faHeart} className="absolute bottom-8 right-5 text-xl text-red-500 hover:text-red-600 cursor-pointer transition-colors duration-300" />
+        </div>
+    );
+}
+
+export default ProductCard;
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Slider from "../components/Home/slider";
+import ProductCard from "../components/Products/ProductCard";
 
 function HomePage(){
     return(
         <div>
             <Slider/>
+            <ProductCard/>
         </div>
     )
 }
