5c2a03fba5a5c9bdaf23d5ddd53371d0ca2f947d
Krishna Sharma <ksharma10_be23@thapar.edu>
2024-08-13 17:16:34 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Route,createBrowserRouter,createRoutesFromElements,RouterProvider } from "react-router-dom";
+import HomePage from "./pages/homepage";
+import LoginRegister from "./components/login/register/LoginRegister";
 import React from "react";
-import Navbar from "./components/Header/navbar";
-import Slider from "./components/Home/slider"
+import MainLayout from "./Layouts/MainLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout/>}>
+    <Route index element={<HomePage/>}/>
+    <Route path='/login' element={<LoginRegister/>}/>
+  </Route>)
+); 
 
 function App(){
   return(
     <div>
-      <Navbar />
-      <Slider />
+      <RouterProvider router={router}></RouterProvider>
     </div>
   )
 }
---
+++ src/pages/homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Slider from "../components/Home/slider";
+
+function HomePage(){
+    return(
+        <div>
+            <Slider/>
+        </div>
+    )
+}
+
+export default HomePage;
