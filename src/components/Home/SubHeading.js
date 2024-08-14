624cbc5cfaf8996222b5c8608d934ac818d8dfb3
Krishna Sharma <ksharma10_be23@thapar.edu>
2024-08-14 23:56:39 +0530

    added sub-heading

---
+++ src/components/Home/SubHeading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SubHeading = () => {
+  return (
+    <h2 className="text-center text-5xl font-bold mt-8 bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-teal-500">Discover our World of Products</h2>
+  );
+};
+
+export default SubHeading;
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Slider from "../components/Home/slider";
 import ProductCard from "../components/Products/ProductCard";
+import SubHeading from "../components/Home/SubHeading";
 
 function HomePage(){
     return(
         <div>
             <Slider/>
-            <ProductCard/>
+            <SubHeading/>
         </div>
     )
 }
